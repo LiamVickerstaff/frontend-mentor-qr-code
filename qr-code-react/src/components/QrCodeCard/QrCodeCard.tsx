33de299a884960d3dfe4cca483d2ea54b342bf78
Liam V <liamvickerstaff050@gmail.com>
2025-05-26 13:53:57 +1000

    fix image url for deployment

--- qr-code-react/src/components/QrCodeCard/QrCodeCard.tsx
+++ qr-code-react/src/components/QrCodeCard/QrCodeCard.tsx
@@ -1,13 +1,10 @@
 import styles from "./QrCodeCard.module.css";
+import myImage from "../../assets/image-qr-code.png";
 
 export default function QrCodeCard() {
   return (
     <div className={`${styles.QrCodeContainer}`}>
-      <img
-        src="/src/assets/image-qr-code.png"
-        alt="QR code"
-        className={`${styles.QrCodeImage}`}
-      />
+      <img src={myImage} alt="QR code" className={`${styles.QrCodeImage}`} />
       <div className={`${styles.textContainer}`}>
         <h4 className={`${styles.subheader} text-outfit-bold`}>
           Improve your front-end skills by building projects
