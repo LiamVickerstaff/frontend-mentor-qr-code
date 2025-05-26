bd38bcddc89a01df294636cdb3c3b3df45688a7c
Liam V <liamvickerstaff050@gmail.com>
2025-05-26 13:05:49 +1000

    New QR Code Component

---
+++ qr-code-react/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import QrCodeCard from "./components/QrCodeCard/QrCodeCard";
+
+export default function App() {
+  return (
+    <div className="appContainer">
+      <QrCodeCard />
+    </div>
+  );
+}
---
+++ qr-code-react/src/components/QrCodeCard/QrCodeCard.tsx
@@ -0,0 +1,22 @@
+import styles from "./QrCodeCard.module.css";
+
+export default function QrCodeCard() {
+  return (
+    <div className={`${styles.QrCodeContainer}`}>
+      <img
+        src="/src/assets/image-qr-code.png"
+        alt="QR code"
+        className={`${styles.QrCodeImage}`}
+      />
+      <div className={`${styles.textContainer}`}>
+        <h4 className={`${styles.subheader} text-outfit-bold`}>
+          Improve your front-end skills by building projects
+        </h4>
+        <p className={`${styles.paragraph} text-outfit-regular`}>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
